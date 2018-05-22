bbe03db50932ca33ae5a0d0e3f1b8d818344a06a
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2018-05-23 00:55:07 +0800

    many results

--- ann_conv.py
+++ ann_conv.py
@@ -13,34 +13,44 @@ def solveModel():
     t = np.linspace(0, 2000, 2001)
     X, y = sE.generateData(in_width)
     X, y = mE.generateData(t, in_width, nPatients = 20)
+
+    print(X.shape)
+    print(y.shape)
+
+    rY, cY = y.shape
+    yHats = []
     
     if True:
         in_channel = y.shape[1]
         channels   = [10, 8, in_channel]
         padding    = ['SAME', 'SAME', 'VALID']
 
-        X0 = X[:1, :, :]
 
+        print('Fitting a model ...')
         model = ctd.ConvTD(in_width, in_channel, channels, padding)
         model.fit(X, y)
-        yHat = model.predict( X0, nTerms=1500 )
 
-        print(yHat)
+        print('Making Predictions ...')
+        for i in range(5):
+            pos = (rY//20)*i
+            X0 = X[pos:pos+1, :, :]
+            yHat = model.predict( X0, nTerms=1500 )
+            print(yHat)
+            yHats.append(yHat)
 
     print('Generating Plots')
     if True:
-        print(X.shape)
-        print(y.shape)
 
-        rY, cY = y.shape
-
-        for i in tqdm(range(1)):
+        yLims = [(0, 1.5), (0, 0.5), (0, 1)]
+        for i in tqdm(range(5)):
             plt.clf()
             for j in range(cY):
                 plt.subplot(310+j+1)
                 yTP = y[(rY//20)*i : (rY//20)*(i+1)-1, j]
                 plt.plot(yTP, label='actual')
-                plt.plot(yHat[:, j], label='Calculated')
+                plt.plot(yHats[i][:, j], label='Calculated')
+                plt.ylim(yLims[j])
+
             plt.legend(loc='upper right')
             plt.savefig( 'img/y_{:05}.png'.format(i) )
 
